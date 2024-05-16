b6e7c17e819c07b9b8b0e6fd29974bde0f08ea9d
Dudu Oliveira <dudu_olive43@hotmail.com>
2024-05-16 19:17:32 -0300

    cap1

---
+++ parte-2/cap-1/teoria3.py
@@ -0,0 +1,8 @@
+# plt.xlabel('') Adiciona uma legenda para o eixo X
+# plt.ylabel('') Adiciona uma legenda para o eixo Y
+# plt.title('') adiciona um titulo a tabela
+# plt.yticks([]) Altera os dados do eixo Y para exibirem de acordo com a lista que passar no argumento, podemos passar 2 listas para modificar e especificar melhor ainda.
+# plt.xticks([]) Mesma coisa do Y
+
+#Essas personalizações devem ficar antes do comando de .show()
+
